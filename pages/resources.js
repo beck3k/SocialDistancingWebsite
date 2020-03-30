772a788ba0d2c5b001fab329383b8ae3dbdcd2a2
beck3k <beck@beckteck.org>
2020-03-30 12:23:45 -0400

    update

--- pages/resources.js
+++ pages/resources.js
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 import { Typography } from '@material-ui/core';
 
 const Resources = () => {
@@ -10,34 +11,63 @@ const Resources = () => {
         <React.Fragment>
             <Navbar />
             <br /><br />
-            <Box width={1} display="flex" justifyContent="spaceBetween" style={{marginLeft: "24px"}}>
-                <Grid container spacing={1}>
+            <Box width={1} display="flex" style={{marginLeft: "24px"}} flexDirection="column">
+                <Grid container spacing={1} >
                     <Grid container item xs={4} spacing={3}>
-                        <Typography variant="h3" >
+                        <Typography variant="h3" style={{borderBottom: "solid 5px #7ac1e8"}}>
                             Mental Health
                         </Typography>
-                        <Box fontWeight="fontWeightBold" fontSize="24px" style={{backgroundColor: "#94BFE7"}}>
-                            During time of great change and mass panic, a distruption in mental heath is normal
-                        </Box>
-                        <Button href="http://www.nami-wci.org">Resources and more information on how to help</Button>
                     </Grid>
                     <Grid container item xs={4} spacing={3}>
-                        <Typography variant="h3" >
+                        <Typography variant="h3" style={{borderBottom: "solid 5px #7ae8a6"}}>
                             Social Distancing Tips
                         </Typography>
-                        <Box fontWeight="fontWeightBold" fontSize="24px" style={{backgroundColor: "#94BFE7"}}>
-                            One of the best things you can do to help flatten the curve is to practice social distancing
-                        </Box>
-                        <Button href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">Resources and more information on how to help</Button>
                     </Grid>
-                    <Grid container item xs={4} spacing={3}>
-                        <Typography variant="h3" >
+                    <Grid container item xs={4} spacing={3} >
+                        <Typography variant="h3" style={{borderBottom: "solid 5px #9be679"}}>
                             Domestic Violence
                         </Typography>
-                        <Box fontWeight="fontWeightBold" fontSize="24px" style={{backgroundColor: "#94BFE7"}}>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} style={{marginTop: "48px"}}>
+                    <Grid container item xs={4} spacing={3} style={{marginRight: "10px"}}>
+                        <Paper elevation={1} style={{backgroundColor: "#7ac1e8"}}>
+                        <Box fontWeight="fontWeightBold" fontSize="24px" display="flex" justifyContent="center" alignItems="center">
+                            During time of great change and mass panic, a distruption in mental heath is normal
+                        </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid container item xs={4} spacing={3} style={{marginRight: "10px"}}>
+                        <Paper elevation={1}  style={{backgroundColor: "#7ae8a6"}}>
+                        <Box fontWeight="fontWeightBold" fontSize="24px" display="flex" justifyContent="center" alignItems="center">
                             One of the best things you can do to help flatten the curve is to practice social distancing
                         </Box>
-                        <Button href="https://www.futureswithoutviolence.org/get-updates-information-covid-19/">Resources and more information on how to help</Button>
+                        
+                        </Paper>
+                    </Grid>
+                    <Grid container item xs={4} spacing={3} style={{marginRight: "10px"}}>
+                        <Paper elevation={1} style={{backgroundColor: "#9be679"}}>
+                        <Box fontWeight="fontWeightBold" fontSize="24px" display="flex" justifyContent="center" alignItems="center">
+                            Due to the virus, travel restrictions and quarantines are being imposed, leading to those being abused possibly being stuck with their abuser.
+                        </Box>
+                        </Paper>
+                    </Grid>
+                </Grid>
+                <Grid container spacing={2} style={{marginTop: "48px"}}>
+                    <Grid container item xs={4} spacing={3} style={{marginRight: "10px"}}>
+                        <Paper elevation={1}>
+                        <Button href="http://www.nami-wci.org">Resources and more information on how to help</Button>
+                        </Paper>
+                    </Grid>
+                    <Grid container item xs={4} spacing={3} style={{marginRight: "10px"}}>
+                        <Paper elevation={1}>
+                        <Button href="https://www.cdc.gov/coronavirus/2019-ncov/index.html">Chekc out this article on tips and tricks of staying social during this time</Button>
+                        </Paper>
+                    </Grid>
+                    <Grid container item xs={4} spacing={3} style={{marginRight: "10px"}}>
+                        <Paper elevation={1}>
+                        <Button href="https://www.futureswithoutviolence.org/get-updates-information-covid-19/">Check here to see what to do and how to help</Button>
+                        </Paper>
                     </Grid>
                 </Grid>
             </Box>
